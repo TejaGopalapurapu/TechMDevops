847fb29c6eab997d649b0e09babd90b3df6f2c0a
TejaGopalapurapu <gopalapuraputeja@gmail.com>
2023-06-14 12:37:11 +0530

    First Commit

---
+++ Work.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demogit
+{
+    internal class Work
+    {
+        public string sayHello()
+        {
+            Console.WriteLine("Hello");
+            return "Hello";
+        }
+    }
+}
