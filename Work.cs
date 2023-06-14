5ea3eef42599d65d9b318da56e14d0692bf2c845
TejaGopalapurapu <gopalapuraputeja@gmail.com>
2023-06-14 14:15:45 +0530

    Third Commit

--- Work.cs
+++ Work.cs
@@ -10,7 +10,8 @@ namespace Demogit
     {
         public string sayHello()
         {
-            Console.WriteLine("new line after commit");
+            Console.WriteLine("new line after doing commit");
+            Console.WriteLine("new line after second commit");
             Console.WriteLine("Hello");
             return "Hello";
 
